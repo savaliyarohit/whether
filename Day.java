e13cf8220c054ee4a1c887f95dbc33884070d6b2
savaliyarohit <savaliyarohit333@gmail.com>
2022-12-21 18:09:27 +0000

    whether

---
+++ Day.java
@@ -0,0 +1,16 @@
+package whether;
+
+public class Day {
+    public static void main(String[] args) {
+        int a=4;
+        int b=-1;
+        if( a>b) {
+            System.out.println("a is Positive 4");
+        }else if(4<-1) {
+            System.out.println("b is Negative -1");
+        }else{
+            System.out.println("0 is Zero");
+        }
+
+    }
+}
---
+++ Night.java
@@ -0,0 +1,15 @@
+package whether;
+
+public class Night {
+    public static void main(String[] args) {
+        int a=8;
+        int b=-6;
+        if( a<b) {
+            System.out.println("a is Positive 8");
+        }else if(8>-6) {
+            System.out.println("b is Negative -6");
+        }else{
+            System.out.println("0 is Zero");
+        }
+    }
+}
